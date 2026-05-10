61610c52763e7f50ec95b1f808343e173fbb6b5b
muhammadhassanoffi-cs <muhammadhassan.offi@gmail.com>
2026-05-10 17:28:50 +0500

    updating king .cpp file

---
+++ Project1/King.cpp
@@ -0,0 +1,43 @@
+//#include "King.h"
+//
+//King::King() {}
+//King::~King() {}
+//
+//bool King::king1legal(int ix, int iy, int fx, int fy, const char arr[8][8], char tpieces[6], char Tpieces[6], info& other)
+//{
+//	bool valid = false;
+//
+//	if (((fx == ix + 1) || (fx == ix - 1)) && fy == iy)
+//		valid = true;
+//	if (((fy == iy + 1) || (fy == iy - 1)) && fx == ix)
+//		valid = true;
+//	if (fx == ix - 1 && ((fy == iy + 1) || (fy == iy - 1)))
+//		valid = true;
+//	if (((fy == iy - 1) || (fy == iy + 1)) && fx == ix + 1)
+//		valid = true;
+//
+//	if (valid)
+//	{
+//		if (other.getid() == 1)
+//		{
+//			for (int h = 0; h < 6; ++h)
+//			{
+//				if (arr[fx][fy] == Tpieces[h])
+//				{
+//					valid = false;
+//				}
+//			}
+//		}
+//		if (other.getid() == 2)
+//		{
+//			for (int h = 0; h < 6; ++h)
+//			{
+//				if (arr[fx][fy] == tpieces[h])
+//				{
+//					valid = false;
+//				}
+//			}
+//		}
+//	}
+//	return valid;
+//}
