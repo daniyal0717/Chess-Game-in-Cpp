f208653aa1146bf0c18e832a6d5f5f42c2fd0567
Daniyal Choudhary <daniyal07170@gmail.com>
2026-05-10 17:11:10 +0500

    updating pawn class

---
+++ Project1/pawn.h
@@ -0,0 +1,20 @@
+//#pragma once
+//#include <iostream>
+//#include "playerinfo.hpp"
+//
+//using namespace std;
+//
+//class Pawn
+//{
+//private:
+//	bool swapvalid;
+//public:
+//	Pawn();
+//	bool swap1bool();
+//	void swap1pwan(char arr[8][8], int fx, int fy, info& other);
+//
+//	bool pawn1legal(int ix, int iy, int fx, int fy, const char arr[8][8], char tpieces[6], char Tpieces[6], info& other, int enpassantrow, int enpassantcol);
+//	bool pawn2legal(int ix, int iy, int fx, int fy, const char arr[8][8], char tpieces[6], char Tpieces[6], info& other, int enpassantrow, int enpassantcol);
+//
+//	~Pawn();
+//};
