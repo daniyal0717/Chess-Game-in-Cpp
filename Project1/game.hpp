da7e1335ba9f0c89e727bd5e703343673cc6a6f1
umar khan badozai <aouomarkhan4@gmail.com>
2026-05-10 17:00:33 +0500

    updating game header file:

---
+++ Project1/game.hpp
@@ -0,0 +1,20 @@
+//#pragma once
+//#include "board.h"
+//#include "playerinfo.hpp"
+//#include<iostream>
+//
+//using namespace std;
+//
+//
+//class game
+//{
+//
+//private:
+//	playerinfo* pinfo[2];
+//	board chessboard;
+//public:
+//	game();
+//	void run();
+//
+//	~game();
+//};
