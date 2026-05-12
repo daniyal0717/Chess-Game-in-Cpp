ec16d4d9ebb7b1e295c6a2328ea73edf20144f98
Daniyal Choudhary <daniyal07170@gmail.com>
2026-05-13 02:32:44 +0500

    clearing issues with en passant logic and updating GUI color scheme

--- Project1/game.hpp
+++ Project1/game.hpp
@@ -1,20 +1,34 @@
 #pragma once
 #include "board.h"
 #include "playerinfo.hpp"
-#include<iostream>
+#include <SFML/Graphics.hpp>
+#include "renderer.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-
 class game
 {
-
 private:
-	playerinfo* pinfo[2];
+    info* pinfo[2];
 	board chessboard;
+    sf::RenderWindow window;
+    Renderer renderer;
+    bool pieceSelected;
+    int selectedRow, selectedCol;
+    bool gameOver;
+    bool promotionPending;
+    int promotionPlayerId;
+    bool statusFontLoaded;
+    sf::Font statusFont;
+    sf::Text statusText;
+    std::string statusMessage;
+
+    void setStatusMessage(const std::string& message);
+
 public:
 	game();
 	void run();
-
 	~game();
 };
